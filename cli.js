fa20e8b1f7f21078b8c484e29415b4e5c96496d3
Julian Gruber <julian@juliangruber.com>
2019-10-07 11:42:43 +0200

    add prettier-standard

--- cli.js
+++ cli.js
@@ -5,7 +5,8 @@ const libscie = require('libscie-api')
 const meow = require('meow')
 const prompts = require('prompts')
 
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ libscie <action> <input>
 
@@ -14,30 +15,29 @@ const cli = meow(`
 
   Examples
       $ libscie                 [interactive mode]
-`, {
-  flags: {
-    env: {
-      type: 'string',
-      alias: 'e'
+`,
+  {
+    flags: {
+      env: {
+        type: 'string',
+        alias: 'e'
+      }
     }
   }
-})
+)
 
 // env checks
 if (!cli.flags.env) cli.flags.env = '/home/chjh/.libscie'
 
 // if no args go full interactive
 if (cli.input.length === 0) {
-  (async () => {
+  ;(async () => {
     const action = await askAction()
     if (action === 'init') {
       const type = await askType()
       const meta = await askMeta()
 
-      libscie.init(type,
-        cli.flags.env,
-        meta.title,
-        meta.description)
+      libscie.init(type, cli.flags.env, meta.title, meta.description)
     }
 
     if (action === 'cache') {
@@ -55,7 +55,8 @@ if (cli.input.length === 0) {
 }
 
 // add semi-interactive depending on amount of args need to add
-if (cli.input.length === 1) {}
+if (cli.input.length === 1) {
+}
 
 // non-interactive to allow for scripting scripting will be worthwhile
 // to simulate whole environments on the fly
@@ -108,7 +109,8 @@ async function askMeta () {
       type: 'text',
       name: 'description',
       message: 'Description'
-    }]
+    }
+  ]
 
   const res = await prompts(qs)
   return res
@@ -117,7 +119,8 @@ async function askMeta () {
 async function select (type, env) {
   const cache = await libscie.readCache(env)
 
-  const choices = cache.filter(mod => mod.type === type & mod.isOwner)
+  const choices = cache
+    .filter(mod => (mod.type === type) & mod.isOwner)
     .map(choice => {
       const obj = {}
       obj.title = choice.title
