13a45db43781d18feef646299a78dec648911f26
Julian Gruber <julian@juliangruber.com>
2019-10-17 12:27:32 +0200

    add test for init type prompt

--- test.js
+++ test.js
@@ -9,9 +9,22 @@ const hypergraph = args =>
   spawn('node', [`${__dirname}/bin/hypergraph.js`, ...args.split(' ')])
 
 test('init', async t => {
-  const ps = hypergraph('init content')
-  ps.stdin.write('title\n')
-  await match(ps.stdout, 'Description')
-  ps.stdin.write('description\n')
-  await promisify(ps.once.bind(ps))('exit')
+  await t.test('prompt type, title, description', async t => {
+    const ps = hypergraph('init')
+    await match(ps.stdout, 'Profile')
+    ps.stdin.write('\n')
+    await match(ps.stdout, 'Title')
+    ps.stdin.write('title\n')
+    await match(ps.stdout, 'Description')
+    ps.stdin.write('description\n')
+    await promisify(ps.once.bind(ps))('exit')
+  })
+
+  await t.test('prompt title, description', async t => {
+    const ps = hypergraph('init content')
+    ps.stdin.write('title\n')
+    await match(ps.stdout, 'Description')
+    ps.stdin.write('description\n')
+    await promisify(ps.once.bind(ps))('exit')
+  })
 })
