475c0b842c8ff094fe5a8e27c1357f491a298c8c
Julian Gruber <julian@juliangruber.com>
2019-10-17 12:29:02 +0200

    add passing test for default prompt

--- test.js
+++ test.js
@@ -8,6 +8,12 @@ const match = require('stream-match')
 const hypergraph = args =>
   spawn('node', [`${__dirname}/bin/hypergraph.js`, ...args.split(' ')])
 
+test('default', async t => {
+  const ps = hypergraph('')
+  await match(ps.stdout, 'Initialize')
+  ps.kill()
+})
+
 test('init', async t => {
   await t.test('prompt type, title, description', async t => {
     const ps = hypergraph('init')
