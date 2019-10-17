2b0c4848896769de8f26966c133343a427e8e3bf
Julian Gruber <julian@juliangruber.com>
2019-10-17 12:11:02 +0200

    remove flaky delay from init test

--- test.js
+++ test.js
@@ -4,15 +4,26 @@ const { test } = require('tap')
 const { spawn } = require('child_process')
 const { promisify } = require('util')
 
-const sleep = dt => new Promise(resolve => setTimeout(resolve, dt))
-
 const hypergraph = args =>
   spawn('node', [`${__dirname}/bin/hypergraph.js`, ...args.split(' ')])
+
+const match = async (stream, string) =>
+  new Promise(resolve => {
+    let buf = ''
+    const onData = data => {
+      buf += data
+      if (buf.includes(string)) {
+        stream.removeListener('data', onData)
+        resolve()
+      }
+    }
+    stream.on('data', onData)
+  })
 
 test('init', async t => {
   const ps = hypergraph('init content')
   ps.stdin.write('title\n')
-  await sleep(500)
+  await match(ps.stdout, 'Description')
   ps.stdin.write('description\n')
   await promisify(ps.once.bind(ps))('exit')
 })
