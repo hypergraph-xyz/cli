7e363c3bd1364d3a1d0d26f6f0eb090f924fae51
Julian Gruber <julian@juliangruber.com>
2020-02-06 17:43:28 +0100

    autocomplete: fix test on windows

--- test/autocomplete.js
+++ test/autocomplete.js
@@ -5,12 +5,7 @@ const { promisify } = require('util')
 
 test('autocomplete', async t => {
   let cmd = `${__dirname}/../bin/autocomplete.js`
-  let args = ''
-
   // istanbul ignore next
-  if (platform() === 'win32') {
-    [cmd, args] = ['node', cmd]
-  }
-
-  await promisify(exec)(cmd, args)
+  if (platform() === 'win32') cmd = `node ${cmd}`
+  await promisify(exec)(cmd)
 })
