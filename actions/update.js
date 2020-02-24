eb18a51d2025b1ee01933b8b328c5fd0759c86f9
Julian Gruber <julian@juliangruber.com>
2020-02-24 13:09:12 +0100

    update to @p2pcommons/sdk-js@0.5.1

--- actions/update.js
+++ actions/update.js
@@ -42,7 +42,10 @@ exports.handler = async ({ p2p, env, hash, key, value }) => {
   if (key) {
     update[key] = value || ''
     if (key === 'parents') {
-      update[key] = update[key].split(',').filter(Boolean)
+      update[key] = update[key]
+        .split(',')
+        .filter(Boolean)
+        .map(key => `dat://${encode(key)}`)
     }
   } else {
     const { rawJSON } = await p2p.get(hash)
@@ -105,7 +108,7 @@ exports.handler = async ({ p2p, env, hash, key, value }) => {
           message: 'Parents',
           choices: potentialParents.map(mod => ({
             title: mod.rawJSON.title,
-            value: mod.rawJSON.url,
+            value: `dat://${encode(mod.rawJSON.url)}`,
             selected: rawJSON.parents.includes(mod.rawJSON.url)
           }))
         })
