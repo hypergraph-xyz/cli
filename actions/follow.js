7d6c7b2865ee1d22e5b2db54e28f4ab30462ff5f
Julian Gruber <julian@juliangruber.com>
2020-02-25 12:50:08 +0100

    update actions syntax

--- actions/follow.js
+++ actions/follow.js
@@ -4,35 +4,33 @@ const UserError = require('../lib/user-error')
 const log = require('../lib/log')
 const create = require('./create')
 
-module.exports = {
-  title: 'Follow a profile',
-  input: [
-    {
-      name: 'profileUrl',
-      resolve: async () => {
-        throw new UserError('Profile url required')
-      }
+exports.title = 'Follow a profile'
+exports.input = [
+  {
+    name: 'profileUrl',
+    resolve: async () => {
+      throw new UserError('Profile url required')
     }
-  ],
-  handler: async ({ p2p, profileUrl }) => {
-    let writableProfiles = []
-    while (!writableProfiles.length) {
-      const profileMods = await p2p.listProfiles()
-      writableProfiles = profileMods.filter(mod => mod.metadata.isWritable)
-      if (!writableProfiles.length) {
-        log.info('Please create your profile first')
-        await create.handler({ p2p, type: 'profile' })
-      }
+  }
+]
+exports.handler = async ({ p2p, profileUrl }) => {
+  let writableProfiles = []
+  while (!writableProfiles.length) {
+    const profileMods = await p2p.listProfiles()
+    writableProfiles = profileMods.filter(mod => mod.metadata.isWritable)
+    if (!writableProfiles.length) {
+      log.info('Please create your profile first')
+      await create.handler({ p2p, type: 'profile' })
     }
+  }
 
-    await p2p.follow(writableProfiles[0].rawJSON.url, profileUrl)
-    const [key, version] = profileUrl.split('+')
-    if (version) {
-      log.success(
-        `${writableProfiles[0].rawJSON.title} now follows ${key} (v${version})`
-      )
-    } else {
-      log.success(`${writableProfiles[0].rawJSON.title} now follows ${key}`)
-    }
+  await p2p.follow(writableProfiles[0].rawJSON.url, profileUrl)
+  const [key, version] = profileUrl.split('+')
+  if (version) {
+    log.success(
+      `${writableProfiles[0].rawJSON.title} now follows ${key} (v${version})`
+    )
+  } else {
+    log.success(`${writableProfiles[0].rawJSON.title} now follows ${key}`)
   }
 }
--- actions/unfollow.js
+++ actions/unfollow.js
@@ -4,55 +4,53 @@ const UserError = require('../lib/user-error')
 const log = require('../lib/log')
 const prompt = require('../lib/prompt')
 
-module.exports = {
-  title: 'Unfollow a profile',
-  input: [
-    {
-      name: 'profileUrl',
-      resolve: async p2p => {
-        const profiles = await p2p.listProfiles()
-        const writableProfiles = profiles.filter(mod => mod.metadata.isWritable)
-        if (!writableProfiles.length) throw new UserError('No local profile')
-        const following = await Promise.all(
-          writableProfiles[0].rawJSON.follows.map(url => {
-            const [key, version] = url.split('+')
-            const download = false
-            return p2p.clone(key, version, download)
-          })
-        )
-        if (!following.length) throw new UserError('Not following anyone')
-        return prompt({
-          type: 'select',
-          message: 'Select profile module to unfollow',
-          choices: following.map(mod => ({
-            title: `${mod.module.title} (v${mod.version})`,
-            value: `${mod.module.url}+${mod.version}`
-          }))
+exports.title = 'Unfollow a profile'
+exports.input = [
+  {
+    name: 'profileUrl',
+    resolve: async p2p => {
+      const profiles = await p2p.listProfiles()
+      const writableProfiles = profiles.filter(mod => mod.metadata.isWritable)
+      if (!writableProfiles.length) throw new UserError('No local profile')
+      const following = await Promise.all(
+        writableProfiles[0].rawJSON.follows.map(url => {
+          const [key, version] = url.split('+')
+          const download = false
+          return p2p.clone(key, version, download)
         })
-      }
+      )
+      if (!following.length) throw new UserError('Not following anyone')
+      return prompt({
+        type: 'select',
+        message: 'Select profile module to unfollow',
+        choices: following.map(mod => ({
+          title: `${mod.module.title} (v${mod.version})`,
+          value: `${mod.module.url}+${mod.version}`
+        }))
+      })
     }
-  ],
-  handler: async ({ p2p, profileUrl }) => {
-    const profiles = await p2p.listProfiles()
-    const writableProfiles = profiles.filter(mod => mod.metadata.isWritable)
-    if (!writableProfiles.length) throw new UserError('No local profile')
-    const following = await p2p.get(profileUrl)
+  }
+]
+exports.handler = async ({ p2p, profileUrl }) => {
+  const profiles = await p2p.listProfiles()
+  const writableProfiles = profiles.filter(mod => mod.metadata.isWritable)
+  if (!writableProfiles.length) throw new UserError('No local profile')
+  const following = await p2p.get(profileUrl)
 
-    await p2p.unfollow(writableProfiles[0].rawJSON.url, profileUrl)
-    const [, version] = profileUrl.split('+')
-    if (version) {
-      log.success(
-        writableProfiles[0].rawJSON.title,
-        'stopped following',
-        following.rawJSON.name,
-        `(v${version})`
-      )
-    } else {
-      log.success(
-        writableProfiles[0].rawJSON.title,
-        'stopped following',
-        following.rawJSON.name
-      )
-    }
+  await p2p.unfollow(writableProfiles[0].rawJSON.url, profileUrl)
+  const [, version] = profileUrl.split('+')
+  if (version) {
+    log.success(
+      writableProfiles[0].rawJSON.title,
+      'stopped following',
+      following.rawJSON.name,
+      `(v${version})`
+    )
+  } else {
+    log.success(
+      writableProfiles[0].rawJSON.title,
+      'stopped following',
+      following.rawJSON.name
+    )
   }
 }
